2840a03c264d2151935cdcee7cdf9f68219b2251
mostafakamal22 <36553185+mostafakamal22@users.noreply.github.com>
2022-02-02 19:45:28 +0200

    adding project files

---
+++ src/searchPage.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import BooksShelf from './BooksShelf'
+import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+
+export default class SearchPage extends Component {
+  state={
+    query:"",
+    searchedBooks:[]
+  }
+  //handling search query change
+  handleChange = async(query)=> {
+    this.setState({
+      query:query
+    })
+    const searchedBooks = await BooksAPI.search(query);
+    this.setState({
+      searchedBooks:searchedBooks
+    })
+  }
+  //matching the shelves of both (search and home pages) by comparing the Ids of books 
+  matchingShelves = (bookSearchResult)=> {
+    const allbooks=[
+      ...this.props.data.read,
+      ...this.props.data.currentlyReading,
+      ...this.props.data.wantToRead
+    ]
+    const bookId = bookSearchResult.id
+    const findBook =allbooks.find(b=> b.id===bookId)
+    if(findBook!==undefined){
+      bookSearchResult.shelf= findBook.shelf
+      return findBook.shelf
+    }
+    else{
+      return "none"
+    }
+  }
+    render() {
+        return (
+            <div className="search-books">
+            <div className="search-books-bar">
+              <Link to="/" className="close-search"  >Close</Link>
+              <div className="search-books-input-wrapper">
+                <input 
+                  type="text" 
+                  placeholder="Search by title or author"
+                  value={this.state.query} 
+                  onChange={(event)=> this.handleChange(event.target.value)}
+                />
+              </div>
+            </div>
+            <div className="search-books-results">
+              {
+              (typeof this.state.searchedBooks === "undefined" || this.state.searchedBooks.error === "empty query")? 
+               (<div>
+                 {this.state.query !== ""  ? <div className="not-found" >Book Or Auther Not Found</div> : <div></div>} 
+                </div>
+                )
+               :(this.state.searchedBooks.length !== 0  && this.state.query!==""?
+                  (
+                    <BooksShelf 
+                    books={this.state.searchedBooks}
+                    defaultValue={this.matchingShelves} 
+                    update={this.props.update} 
+                    /> 
+                  ):(
+                      <div></div>
+                    )
+                  )
+                }   
+            </div>
+          </div>
+        )
+    }
+}
